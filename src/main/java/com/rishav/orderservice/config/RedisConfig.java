ea9b7f8a0c921a876dcf8667256d876456f01cdc
Rishav Shrivastava <53485725+rishav2220@users.noreply.github.com>
2026-04-02 21:04:02 -0400

    Add src/main/java/com/rishav/orderservice/config/RedisConfig.java

---
+++ src/main/java/com/rishav/orderservice/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.rishav.orderservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.rishav.orderservice.dto.OrderResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, OrderResponse> orderRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, OrderResponse> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        Jackson2JsonRedisSerializer<OrderResponse> serializer =
+                new Jackson2JsonRedisSerializer<>(mapper, OrderResponse.class);
+
+        template.setValueSerializer(serializer);
+        template.setHashValueSerializer(serializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+}
